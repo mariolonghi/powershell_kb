f5d7490bfb6934873433ac2dd1032687005e7f29
mariolonghi <35499178+mariolonghi@users.noreply.github.com>
2020-06-26 16:02:56 -0400

    Create Report_IQP_eml_past_X_Hours.ps1

---
+++ Report_IQP_eml_past_X_Hours.ps1
@@ -0,0 +1,168 @@
+  Start-Transcript -Path "G:\Scripts\Logs\LogForEEL\TranscriptDailyRun_IQP.txt" -Append
+function Get-TimeStamp {return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)}
+
+Write-Host "$(Get-TimeStamp) Start of script by user $env:UserName on the server $env:computername."
+$timestampStartFull = get-date
+$dt = Get-Date -format s
+$DateFormat=$dt.replace(":",".")
+Write-Host "$(Get-TimeStamp) Setting the parameters"
+$StartDate = (Get-date).AddHours(-25)
+$EndDate = (Get-date)
+
+
+Write-Host "$(Get-TimeStamp) Defined Stardate $StartDate and EndDate $EndDate"
+$outFile = "G:\Scripts\Logs\LogForEEL\LogOfDay" + $DateFormat + ".csv"
+#$outFile = "C:\temp\emlFiles\LogOfDay" + $DateFormat + ".csv"
+Write-Host "$(Get-TimeStamp) Setting log file to $outfile"
+Write-Host "$(Get-TimeStamp) Reset the index"
+$indexNumber = 0
+$indexNumberIQPcases = 0
+Write-Host "$(Get-TimeStamp) The $indexNumber should be 0"
+
+$LogEELfolder = "G:\LogForEEL\Drop"
+#$LogEELfolder = "C:\temp\emlFiles\ShippingDocs"
+
+Write-Host "$(Get-TimeStamp) Setting eml file folder to $logeelfolder"
+Write-Host "$(Get-TimeStamp) Setting the category specific folder to $CategoryFolder "
+Write-Host "$(Get-TimeStamp) Loading array with files from $logeelfolder based on filter"
+
+$AllFilesToInspect = Get-ChildItem -Path $logeelfolder | where-object {$_.LastWriteTime -gt $StartDate -AND $_.lastwritetime -lt $EndDate}
+
+
+
+Write-Host "$StartDate and $EndDate"
+Write-Host "$(Get-TimeStamp) Array loaded with $($AllFilesToInspect.Count) files"
+
+Write-Host "$(Get-TimeStamp) Clearing the array for the results"
+
+Write-Host "$(Get-TimeStamp) Logging the report header"
+
+#logging the report header
+$outputArray = "Usage report of old IQP email addresses.<br>Report period: $StartDate to $EndDate<br>Investigated a total of $($AllFilesToInspect.Count) files<br><br>CSV file is also provided attached."
+[PSObject[]]$OutputIQPCases = @()
+Clear-Variable -Name OutputIQPCases
+$allSenders = @()
+
+<#
+$Text =get-content -LiteralPath $AllFilesToInspect[0].fullname -TotalCount 20
+$text | Where-Object {$_.Contains("x-receiver: ")}
+
+#>
+
+
+
+Write-Host "$(Get-TimeStamp) File inspection starting"
+foreach ($filetoInspect in $AllFilesToInspect){
+	$indexNumber += 1
+    Write-Host "$(Get-TimeStamp) Loop $indexNumber - Start of the loop"
+
+    #the Contains switch is case senstive
+    Write-Host "$(Get-TimeStamp) Loop $indexNumber - Reading file $($filetoInspect.FullName) with size $($filetoInspect.Length)"
+    $text = Get-Content -LiteralPath $filetoInspect.FullName -TotalCount 200
+	$ReceiverContainer = $text | Where-Object {$_.Contains("x-receiver: ")}
+    $SenderContainer = $text | Where-Object {$_.Contains("x-sender: ")}
+    $CategoryContainer = "EMPTY"
+	Write-Host "$(Get-TimeStamp) Loop $indexNumber - starting category for $($filetoInspect.FullName) "
+    if ($ReceiverContainer -like "*bp.restricted@infinet.infineum.com*"){ $CategoryContainer = "IQP111_BP"}
+    elseif ($ReceiverContainer -like "*iqp.openaccess@infinet.infineum.com"){ $CategoryContainer = "IQP111_General"}
+    elseif ($ReceiverContainer -like "*other.type4restricted@infinet.infineum.com"){ $CategoryContainer = "IQP111_OtherType4"}
+    elseif ($ReceiverContainer -like "*shell.restricted@infinet.infineum.com"){ $CategoryContainer = "IQP111_Shell"}
+    elseif ($ReceiverContainer -like "*shell.lcc.pcmo.restricted@infinet.infineum.com"){ $CategoryContainer = "IQP111_ShellLCCPCMO"}
+    elseif ($ReceiverContainer -like "*shell.lcc.tpeo.restricted@infinet.infineum.com"){ $CategoryContainer = "IQP111_ShellLCCTPEO"}
+    elseif ($ReceiverContainer -like "*exxonmobil.restricted@infinet.infineum.com"){ $CategoryContainer = "IQP111_XOM"}
+    elseif ($ReceiverContainer -like "*IQP111_FuelsCat5@infinet.infineum.com"){ $CategoryContainer = "IQP111_FuelsCat5"}
+	elseif ($ReceiverContainer -like "*IQP1.11Cat5@infinet.infineum.com"){ $CategoryContainer = "IQP111_FuelsCat5"}
+    else { $CategoryContainer = "Any_Other"}
+    
+    Write-Host "$(Get-TimeStamp) Loop $indexNumber - Running the IQP check"
+    if ($CategoryContainer -like "IQP*"){
+        Write-Host "$(Get-TimeStamp) Loop $indexNumber - Positive IQP loop" -ForegroundColor Blue -BackgroundColor White
+        $ReceiverEmail = $ReceiverContainer.ToLower().Replace("x-receiver: ","").Replace("@infinet.infineum.com","")
+        $SenderEmail = $SenderContainer.ToLower().Replace("x-sender: ","")
+        Write-Host "$(Get-TimeStamp) Loop $indexNumber - Writing result to array" -ForegroundColor Blue -BackgroundColor White
+        
+        
+        write-host "$SenderEmail"
+        
+               
+        $allSenders += $SenderEmail + ";"
+        
+        $Email = @{Name = $($filetoInspect.Name)
+        Receiver = $ReceiverEmail
+        Sender = $SenderEmail
+        Category = $CategoryContainer
+        Date = $text | Where-Object {$_.Contains("Date: ")} 
+        }
+        
+        $emailobject = New-Object -TypeName PsObject -Property $email
+        
+        $OutputIQPCases += $emailobject
+        
+        
+        $indexNumberIQPcases += 1
+        Write-Host "$(Get-TimeStamp) Loop $indexNumber - End of positive IQP check" -ForegroundColor Blue -BackgroundColor White
+        }
+    else {
+        Write-Host "$(Get-TimeStamp) Loop $indexNumber - Not an IQP item. $ReceiverContainer"
+        }
+    
+    Write-Host "$(Get-TimeStamp) Loop $indexNumber - End of loop, count of IQP cases: $indexNumberIQPcases" -ForegroundColor DarkBlue -BackgroundColor White
+    #This is troubleshooting break
+    #if ($indexNumber -gt 200){break}
+
+}
+
+Write-Host "$(Get-TimeStamp) Ended loop through all $indexNumber files"
+$outputArray += "Number of IQP Cases: $indexNumberIQPcases out of $indexNumber EEL files<br><br>"
+$OutputIQPCases | Export-Csv -Path $outFile
+Write-Host "The type of the OutputIQPCases $($OutputIQPCases.GetType())"
+$OutputIQPCases.gettype()
+
+
+
+#Checking the time elapsed
+$timestampEndFull = Get-Date
+Write-Host "$(Get-TimeStamp) Calculating the elapsed $timestampEndFull MINUS $timestampStartFull"
+$elapsedTime = $timestampEndFull - $timestampStartFull
+Write-Host "$(Get-TimeStamp) Calculated the elapsed time: $elapsedTime"
+
+#variables for the emailing function 
+Write-Host "$(Get-TimeStamp) Setting up email variables including body"
+$SmtpServer = "smtp.infineum.com"
+$MailFrom = "Mario.Longhi@infineum.com"
+$MailtToITAdmin	= "Mario.Longhi@infineum.com" 
+$MailtToIQPAdmin = "Brian.Jordan@infineum.com"
+
+
+$BottomoutputArray += "These are all the senders in order (for easy copy/paste):<br>"
+$BottomoutputArray += $allsenders | Sort-Object | Get-Unique
+$BottomoutputArray += "<br><br>Elapsed time: $elapsedTime<br>Time stamp: $dt by $env:UserName on the server $env:computername."
+
+$PostContent = $BottomoutputArray | Out-String
+$preContent = $outputArray | Out-String
+
+$FinaloutputArray += $OutputIQPCases | ConvertTo-Html -postContent $PostContent -PreContent $preContent -Title "IQP 1.11 Old EEL Report"
+
+$MailBody += $FinaloutputArray | Out-String
+
+$Mailbody = $MailBody.Replace("<table>",'<table border="1" cellpadding="1" cellspacing="1">')
+
+
+
+if ($indexNumberIQPcases -gt 0){
+    Write-Host "$(Get-TimeStamp) There are $indexNumberIQPcases IQP cases today."
+    Write-Host "$(Get-TimeStamp) Sending email on $smtpServer"
+    $MailSubject = "EEL IQP Report - $indexNumberIQPcases cases of wrong use of IQP emails today - $($timestampStartFull.ToShortDateString())"
+    Send-MailMessage -To $MailtToIQPAdmin -Cc $MailtToITAdmin -from $MailFrom -Subject $MailSubject -SmtpServer $SmtpServer -Body $MailBody -Priority Low -BodyAsHtml -Attachments $outFile
+}
+else {
+    Write-Host "$(Get-TimeStamp) There are 0 cases today. Index of IQP: $indexNumberIQPcases."
+    $MailSubject = "EEL IQP Report - $indexNumberIQPcases cases of wrong use of IQP emails today - $($timestampStartFull.ToShortDateString())"
+    $MailBody = "Investigated a total of $($AllFilesToInspect.Count) files and there were no IQP cases to report on the period: $StartDate to $EndDate<br><br>Elapsed time: $elapsedTime<br>Time stamp: $dt by $env:UserName on the server $env:computername."
+    Send-MailMessage -To $MailtToIQPAdmin -Cc $MailtToITAdmin -from $MailFrom -Subject $MailSubject  -SmtpServer $SmtpServer -Body $MailBody -Priority Low # -Attachments $outFile
+}
+
+Write-Host "$(Get-TimeStamp) End of Script"
+
+
+Stop-transcript
